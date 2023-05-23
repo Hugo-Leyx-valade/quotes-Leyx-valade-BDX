59eceac50ccedecae2c62b09fa10f1bf1695d7a2
Victor <victor.dallon@efrei.net>
2023-05-23 08:21:01 +0200

    ajout de add quote

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def menu():
 	print("\n==== Programming Quotes ====")
 	print("1. Random quote")
 	print("2. All quotes")
-	print("3. Exit")
+    print("3. Add quote")
+	print("4. Exit")
 
 
 def main():
@@ -19,7 +20,9 @@ def main():
 			print_quote(random_quote(quotes))
 		elif choice == "2":
 			view_quotes(quotes)
-		elif choice == "3":
+        elif choice == "3":
+            add_quote(quotes,"quotes.txt")
+		elif choice == "4":
 			print("Good bye...")
 			break
 		else:
